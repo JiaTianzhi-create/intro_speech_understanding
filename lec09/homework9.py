f7ed061460b1719fa46b3472cf485f2b39182597
JiaTianzhi <msymaryx@gmail.com>
2026-03-01 07:00:44 +0800

    Homework 9 completed - VAD + vowel models + cosine similarity recognition

--- lec09/homework9.py
+++ lec09/homework9.py
@@ -13,7 +13,42 @@ def VAD(waveform, Fs):
     segments (list of arrays) - list of the waveform segments where energy is 
        greater than 10% of maximum energy
     '''
-    raise RuntimeError("You need to change this part")
+    frame_length = int(0.025 * Fs)
+    step = int(0.01 * Fs)
+    
+
+    num_frames = (len(waveform) - frame_length) // step + 1
+    frames = np.lib.stride_tricks.sliding_window_view(
+        waveform, frame_length)[::step]
+    
+
+    energies = np.sum(frames ** 2, axis=1)
+    max_energy = np.max(energies)
+    thresh = 0.1 * max_energy
+    
+
+    voiced = energies > thresh
+    
+
+    segments = []
+    start = None
+    for i in range(len(voiced)):
+        if voiced[i]:
+            if start is None:
+                start = i
+        else:
+            if start is not None:
+                seg_start = start * step
+                seg_end = (i - 1) * step + frame_length
+                segments.append(waveform[seg_start:seg_end])
+                start = None
+    if start is not None:
+        seg_start = start * step
+        seg_end = (len(voiced) - 1) * step + frame_length
+        segments.append(waveform[seg_start:seg_end])
+    
+    return segments
+
 
 def segments_to_models(segments, Fs):
     '''
@@ -29,7 +64,33 @@ def segments_to_models(segments, Fs):
     @returns:
     models (list of arrays) - average log spectra of pre-emphasized waveform segments
     '''
-    raise RuntimeError("You need to change this part")
+    models = []
+    frame_length = int(0.004 * Fs)   # 4ms
+    step = int(0.002 * Fs)           # 2ms
+    
+    for seg in segments:
+        if len(seg) < frame_length:
+            continue 
+        
+        # Pre-emphasis
+        preemph = np.append(seg[0], seg[1:] - 0.97 * seg[:-1])
+        
+     
+        num_frames = (len(preemph) - frame_length) // step + 1
+        frames = np.lib.stride_tricks.sliding_window_view(
+            preemph, frame_length)[::step]
+        
+     
+        mstft = np.abs(np.fft.fft(frames, axis=1))
+        
+        low_half = mstft[:, :frame_length // 2]
+        
+
+        model = np.mean(low_half, axis=0)
+        models.append(model)
+    
+    return models
+
 
 def recognize_speech(testspeech, Fs, models, labels):
     '''
@@ -47,6 +108,23 @@ def recognize_speech(testspeech, Fs, models, labels):
     sims (Y-by-K array) - cosine similarity of each model to each test segment
     test_outputs (list of strings) - recognized label of each test segment
     '''
-    raise RuntimeError("You need to change this part")
+    test_segments = VAD(testspeech, Fs)
+    test_models = segments_to_models(test_segments, Fs)
+    
+    Y = len(models)   
+    K = len(test_models)  
+    sims = np.zeros((Y, K))
+    
+    for i in range(Y):
+        for j in range(K):
+            a = models[i]
+            b = test_models[j]
+            if np.linalg.norm(a) == 0 or np.linalg.norm(b) == 0:
+                sims[i, j] = 0
+            else:
+                sims[i, j] = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    
 
-
+    test_outputs = [labels[np.argmax(sims[:, j])] for j in range(K)]
+    
+    return sims, test_outputs
